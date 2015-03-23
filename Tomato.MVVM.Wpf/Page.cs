d0dd01d1a8dda8130e7be25ecc5636c2e9f8233c
sunnycase <sunnycase@live.cn>
2015-03-23 15:01:32 +0800

    add mvvm for wpf

---
+++ Tomato.MVVM.Wpf/Page.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tomato.MVVM.Wpf
+{
+    public class Page : System.Windows.Controls.Page
+    {
+        public dynamic ViewBag
+        {
+            get { return GetValue(Window.ViewBagProperty.DependencyProperty); }
+        }
+
+        static Page()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Page), new FrameworkPropertyMetadata(typeof(Page)));
+        }
+    }
+}
---
+++ Tomato.MVVM.Wpf/Window.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tomato.MVVM.Wpf
+{
+    public class Window : System.Windows.Window
+    {
+        public static readonly DependencyPropertyKey ViewBagProperty = DependencyProperty.RegisterReadOnly("ViewBag", typeof(ExpandoObject), typeof(Window), new PropertyMetadata(new ExpandoObject()));
+
+        public dynamic ViewBag
+        {
+            get { return GetValue(ViewBagProperty.DependencyProperty); }
+        }
+
+        static Window()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata(typeof(Window)));
+        }
+    }
+}
