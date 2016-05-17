ed9c2026a9051e344b521fe42c0a9973820aea63
sunnycase <sunnycase@live.cn>
2016-05-17 18:34:20 +0800

    修复操作队列

---
+++ Tomato.Mvvm.Xaml/Converters/DummyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tomato.Mvvm.Converters
+{
+    public sealed class DummyConverter : ValueConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
---
+++ Tomato.Threading/Test/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Tomato.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly OperationQueue _operationQueue = new OperationQueue(1);
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await _operationQueue.Queue(async () =>
+            {
+                await Task.Delay(1000);
+                Math.Abs(234);
+                await Task.Delay(1000);
+                Math.Abs(234);
+                await Task.Delay(1000);
+                Math.Abs(234);
+                await Task.Delay(1000);
+                Math.Abs(234);
+                await Task.Delay(1000);
+            });
+        }
+    }
+}
